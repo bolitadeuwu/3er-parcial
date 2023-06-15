4ba5c6af154e50609b8f015b155c0774d2e6aaae
Erika Daniela <124924346+bolitadeuwu@users.noreply.github.com>
2023-06-15 13:35:19 -0600

    Add files via upload

---
+++ 15 Mayo/WinFormsApp1/Form1.vb
@@ -0,0 +1,42 @@
+﻿Public Class Form1
+    Dim color As Long
+    Dim p As Integer
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        color = RGB(0, 0, 0) 'negro
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        color = RGB(255, 0, 0) 'rojo
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        color = RGB(255, 192, 203) 'rosa
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        color = RGB(255, 255, 0) 'amarillo
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        PictureBox1.Height = InputBox("Escribe el grosor del puntero:", "Altura")  'grosor puntero
+        PictureBox1.Width = InputBox("Escribe el grosor del puntero:", "Ancho")
+
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(button As Integer, shift As Integer, X As Single, Y As Single)
+        If button = 1 Then
+            PictureBox1.BackColor = BackColor
+        End If
+
+        If button = 2 Then
+            PictureBox1.BackColor = BackColor
+        End If
+        If button = 3 Then
+            PictureBox1.BackColor = BackColor
+        End If
+        If button = 4 Then
+            PictureBox1.BackColor = BackColor
+        End If
+    End Sub
+
+End Class
---
+++ 15 Mayo/WinFormsApp2/Form1.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form1
+    Private line As New List(Of Point)
+
+    Private Sub Form1_MouseDown(sender As Object, e As MouseEventArgs) Handles Me.MouseDown
+        line = New List(Of Point)
+        line.Add(e.Location)
+    End Sub
+
+    Private Sub Form1_MouseMove(sender As Object, e As MouseEventArgs) Handles Me.MouseMove
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            line.Add(e.Location)
+            Me.Invalidate()
+        End If
+    End Sub
+
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+        If line.Count > 1 Then
+            e.Graphics.DrawLines(Pens.Black, line.ToArray)
+        End If
+    End Sub
+
+End Class
